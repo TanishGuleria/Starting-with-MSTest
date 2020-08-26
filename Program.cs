9f57e523640fa17ebe3762c47e922c3116973882
TanishGuleria <38018938+TanishGuleria@users.noreply.github.com>
2020-08-27 02:02:58 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Starting_with_MSTest
+{
+    public class Program
+    {
+        public static void take(ref int a, ref int b)
+        {
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+        }
+        public static int add(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+
+
+            int a = 0;
+            int b = 0;
+
+            take(ref a, ref b);
+
+
+            Console.WriteLine(add(a, b));
+
+
+        }
+    }
+}
